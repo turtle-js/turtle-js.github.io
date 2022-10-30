35b60c08c7dc3f17d3e4f0fc83aad2d0871672f9
joker876 <42680429+joker876@users.noreply.github.com>
2022-10-30 17:53:16 +0100

    create local storage service

---
+++ src/app/services/local-storage/local-storage.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LocalStorageService {
+
+    constructor() { }
+
+    /**
+     * Loads some data from user's local storage.
+     * @param item the name of the key to load
+     * @returns the data saved in the local storage.
+     * * If no data is found, `null` is returned.
+     * * If data is an object, it is parsed using `JSON.parse()` and returned.
+     * * If data is a number, it is parsed using `Number()` and returned.
+     * * If data is a boolean or a string, it is returned unmodified.
+     */
+    public load(item: string): null | object | number | boolean | string {
+        let data = localStorage.getItem(item);
+        if (!data || data == 'null') return null;
+        if (data.trim().match(/^[\[\{]/)) return JSON.parse(data);
+        if (Number(data)) return Number(data);
+        if (data.match(/^(?:true|false)$/)) return data == 'true';
+        return data;
+    }
+
+    /**
+     * Saves some data to user's local storage.
+     * @param key the name of the key to save to
+     * @param data the data to be saved
+     * @returns `true` if the save was succesful, otherwise `false`.
+     */
+    public save(key: string, data: any) {
+        try {
+            let dataStr!: string;
+            if (typeof data == 'string') dataStr = data;
+            else dataStr = JSON.stringify(data);
+
+            localStorage.setItem(key, dataStr);
+            return true;
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    }
+}
