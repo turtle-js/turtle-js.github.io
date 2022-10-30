6882cfcd290609922938bd907fc668965e65893f
joker876 <42680429+joker876@users.noreply.github.com>
2022-10-30 20:14:42 +0100

    simplify mat-icon component structure

--- src/app/components/mat-icon/mat-icon.component.ts
+++ src/app/components/mat-icon/mat-icon.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'mat-icon',
-  templateUrl: './mat-icon.component.html'
+    selector: 'mat-icon',
+    template: `
+    <span class="material-symbols-outlined">
+        <ng-content></ng-content>
+    </span>`,
+    styleUrls: ['./mat-icon.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class MatIconComponent {}
