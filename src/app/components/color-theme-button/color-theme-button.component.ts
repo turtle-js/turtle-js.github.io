902b89bdbe3f0581305fc580ef317812adbf1e7a
joker876 <42680429+joker876@users.noreply.github.com>
2022-10-31 17:46:41 +0100

    refactor theme button comp. to use a service
    the previous implementation used an observable inside the component. This was dumb.

--- src/app/components/color-theme-button/color-theme-button.component.ts
+++ src/app/components/color-theme-button/color-theme-button.component.ts
@@ -1,98 +1,20 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { Subject } from 'rxjs';
-import { LocalStorageService } from './../../services/local-storage/local-storage.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ThemeService } from './../../services/theme/theme.service';
 
-type ColorTheme = 'light' | 'dark';
-type ColorThemeOption = 'system' | ColorTheme;
 
 @Component({
     selector: 'tjs-color-theme-button',
     templateUrl: './color-theme-button.component.html',
     styleUrls: ['./color-theme-button.component.scss']
 })
-export class ColorThemeButtonComponent implements OnInit {
-    private _theme!: ColorThemeOption;
-    private _themeSubject = new Subject<ColorThemeOption>();
-    public theme = this._themeSubject.asObservable();
-
-    static readonly allThemes = [
-        'dark',
-        'light',
-    ];
-
-    private _bodyClass!: string;
+export class ColorThemeButtonComponent {
+    theme = this.themeService.theme;
 
     constructor(
-        private localStorageService: LocalStorageService,
-        private renderer: Renderer2
-    ) { }
-
-    public get themeClass(): ColorTheme {
-        let cls: ColorTheme;
-        switch (this._theme) {
-            case 'system':
-                if (!window.matchMedia || window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                    cls = 'dark';
-                }
-                else {
-                    cls = 'light';
-                }
-                break;
-            default:
-                cls = this._theme;
-                break;
-        }
-        return cls;
-    }
-    public get getTheme(): ColorThemeOption {
-        return this._theme;
-    }
-
-    ngOnInit(): void {
-        let themeFromLS = this.localStorageService.load('theme') as ColorThemeOption | null;
-        if (!themeFromLS) {
-            this._theme = 'system';
-            this.saveToLS();
-            return;
-        }
-        else {
-            this._theme = themeFromLS;
-        }
-        this._themeSubject.next(this._theme);
-        this._setBodyClass();
-    }
-    private _setBodyClass() {
-        //get class
-        const classToSet = 'theme-' + this.themeClass;
-        //find out which class to (not) remove and update <body>
-        const allClasses = ColorThemeButtonComponent.allThemes.map(cls => 'theme-' + cls);
-        const clsIndex = allClasses.indexOf(classToSet);
-        allClasses.splice(clsIndex, 1);
-        for (const cls of allClasses) {
-            this.renderer.removeClass(document.body, cls);
-        }
-        //add the correct class to <body>
-        if (this._bodyClass != classToSet) {
-            this.renderer.addClass(document.body, classToSet);
-            this._bodyClass = classToSet;
-        }
-    }
-    changeTheme() {
-        switch (this._theme) {
-            case 'system':
-                this._theme = 'dark';
-                break;
-            case 'dark':
-                this._theme = 'light';
-                break;
-            case 'light':
-                this._theme = 'system';
-                break;
-        }
-        this.saveToLS();
-        this._setBodyClass();
-    }
-    saveToLS() {
-        this.localStorageService.save('theme', this._theme);
+        private themeService: ThemeService
+    ) {}
+    
+    nextTheme() {
+        this.themeService.nextTheme();
     }
 }
---
+++ src/app/services/theme/theme.service.ts
@@ -0,0 +1,89 @@
+import { Injectable, Renderer2, OnInit, Inject, RendererFactory2 } from '@angular/core';
+import { LocalStorageService } from './../local-storage/local-storage.service';
+import { BehaviorSubject } from 'rxjs';
+
+export type ColorTheme = 'light' | 'dark';
+export type ColorThemeOption = 'system' | ColorTheme;
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ThemeService {
+    private _theme!: ColorThemeOption;
+    private _themeSubject = new BehaviorSubject<ColorThemeOption>(this._theme);
+    public theme = this._themeSubject.asObservable();
+
+    private _bodyClass?: string;
+
+    private renderer!: Renderer2;
+
+    constructor(
+        private localStorageService: LocalStorageService,
+        private rendererFactory: RendererFactory2,
+    ) {
+        this.renderer = this.rendererFactory.createRenderer(document.body, null);
+
+        let themeFromLS = this.localStorageService.loadString('theme') as ColorThemeOption | null;
+        if (!themeFromLS) {
+            this._theme = 'system';
+        }
+        else {
+            this._theme = themeFromLS;
+        }
+        this._onChange();
+        this._setBodyClass();
+    }
+
+    static readonly allThemes = [
+        'dark',
+        'light',
+    ];
+    static themeToClass(theme: ColorThemeOption): ColorTheme {
+        switch (theme) {
+            case 'system':
+                if (!window.matchMedia || window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    return 'dark';
+                }
+                return 'light';
+        }
+        return theme;
+    }
+    private get _themeClass(): ColorTheme {
+        return ThemeService.themeToClass(this._theme);
+    }
+    private _setBodyClass() {
+        //get class
+        const classToSet = 'theme-' + this._themeClass;
+        //find out which class to (not) remove and update <body>
+        const allClasses = ThemeService.allThemes.map(cls => 'theme-' + cls);
+        const clsIndex = allClasses.indexOf(classToSet);
+        allClasses.splice(clsIndex, 1);
+        for (const cls of allClasses) {
+            this.renderer.removeClass(document.body, cls);
+        }
+        //add the correct class to <body>
+        if (this._bodyClass != classToSet) {
+            this.renderer.addClass(document.body, classToSet);
+            this._bodyClass = classToSet;
+        }
+    }
+    nextTheme() {
+        switch (this._theme) {
+            case 'system':
+                this._theme = 'dark';
+                break;
+            case 'dark':
+                this._theme = 'light';
+                break;
+            case 'light':
+                this._theme = 'system';
+                break;
+        }
+        this._onChange();
+        this._setBodyClass();
+    }
+    private _onChange() {
+        this._themeSubject.next(this._theme);
+        this.localStorageService.save('theme', this._theme);
+    }
+}
