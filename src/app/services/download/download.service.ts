b97a146527b3df9294bec67025836e636b14fa10
joker876 <42680429+joker876@users.noreply.github.com>
2022-10-31 17:45:41 +0100

    create download service

---
+++ src/app/services/download/download.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DownloadService {
+    download(text: string, filename: string) {
+        const file = new Blob([text], { type: 'text/javascript' });
+        const aEl = document.createElement("a");
+        const url = URL.createObjectURL(file);
+        aEl.href = url;
+        aEl.download = filename;
+        document.body.appendChild(aEl);
+        aEl.click();
+        setTimeout(function () {
+            document.body.removeChild(aEl);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
